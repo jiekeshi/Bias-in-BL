66183b1b28b2b8eb1243e84484507dc6baac5988
jiekeshi <jiekeshi125@gmail.com>
2021-11-15 20:20:07 +0800

    chore: script to download data

--- data/download.sh
+++ data/download.sh
@@ -1,4 +1,8 @@
-pwd
+gdown https://drive.google.com/uc?id=1MaueUJCcPLHaJux8jTyVdBVhc55pO_y0
+gdown https://drive.google.com/uc?id=1j5geXJQeIBGN-fcULYV-ZgEAeByjGofS
+unzip Bias_1_not_misclassified.zip
+unzip Bias_1_misclassified.zip
+mkdir codebase
 cd codebase
 git clone https://github.com/apache/ambari.git
 git clone https://github.com/apache/bigtop.git
